84fcb0bd3ccb611df5ff79efec4662f31be3a570
aytajquseynova <aytajguseyn@gmail.com>
2023-11-04 01:31:25 +0400

    burger menu

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbName = "burgerorder";
+
+
+try {
+    $dsn = "mysql:host=" . $host . ";dbname=" . $dbName;
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    echo "Baglanti yapildi" . "<br>";
+} catch (PDOException $e) {
+    echo "Baglanti hatasi:" . $e->getMessage();
+}
+?>
